f84caff62e90f0fb96830358724937b94fb73288
galvanitic <develop@rglvn.com>
2020-11-19 19:52:31 -0600

    Completed linked-list node implementation with shared_ptr

--- Node.cpp
+++ Node.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Node.h"
+Node::Node() : next(nullptr) {
+  line = "";
+}
+void Node::setLine(const std::string &aLine) {
+  line = aLine;
+}
+void Node::setNext(std::shared_ptr<Node>* nextNodePtr) {
+  next = nextNodePtr;
+}
+
+std::string Node::getLine() const {
+  return line;
+}
+
+std::shared_ptr<Node>* Node::getNext() const {
+  return next;
+}
